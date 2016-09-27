a8d0c6616ade73289a6163ba431ade83605a41aa
Halliburton, Sean <Sean.Halliburton@nordstrom.com>
2016-09-27 14:32:53 -0700

    clearing todo msgs

--- sqlrunner/sql/public/deduplicate/com-nordstrom-elwin-exposures.sql
+++ sqlrunner/sql/public/deduplicate/com-nordstrom-elwin-exposures.sql
@@ -1,19 +1,19 @@
 -- copy-com-elwin-exposures.sql
 
-DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures; -- todo: replace placeholder name
-DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id; -- todo: replace placeholder name
-DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining; -- todo: replace placeholder name
+DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures;
+DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id;
+DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining;
 
 -- (a) list all root_id that occur more than once in the target table
 
-CREATE TABLE duplicates.tmp_com_nordstrom_elwin_exposures_id -- todo: replace placeholder name
+CREATE TABLE duplicates.tmp_com_nordstrom_elwin_exposures_id
   DISTKEY (root_id)
   SORTKEY (root_id)
-AS (SELECT root_id FROM (SELECT root_id, COUNT(*) AS count FROM public.com_nordstrom_elwin_exposures GROUP BY 1) WHERE count > 1); -- todo: replace placeholder name
+AS (SELECT root_id FROM (SELECT root_id, COUNT(*) AS count FROM public.com_nordstrom_elwin_exposures GROUP BY 1) WHERE count > 1);
 
 -- (b) create a new table with these events and deduplicate as much as possible using GROUP BY
 
-CREATE TABLE duplicates.tmp_com_nordstrom_elwin_exposures -- todo: replace placeholder name
+CREATE TABLE duplicates.tmp_com_nordstrom_elwin_exposures
   DISTKEY (root_id)
   SORTKEY (root_id)
 AS (
@@ -37,11 +37,9 @@ AS (
        parameter_value,
        elwin_id
 
-    -- todo: add all other columns
-
-  FROM public.com_nordstrom_elwin_exposures -- todo: replace placeholder name
-  WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id) -- todo: replace placeholder name
-  GROUP BY 1,2,3,4,5,7,8,9, 10,11,12,13,14 -- todo: add all remaining columns (except root_tstamp)
+  FROM public.com_nordstrom_elwin_exposures
+  WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id)
+  GROUP BY 1,2,3,4,5,7,8,9, 10,11,12,13,14
 
 );
 
@@ -49,27 +47,27 @@ AS (
 
 BEGIN;
 
-  DELETE FROM public.com_nordstrom_elwin_exposures WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id); -- todo: replace placeholder name
-  INSERT INTO public.com_nordstrom_elwin_exposures (SELECT * FROM duplicates.tmp_com_nordstrom_elwin_exposures); -- todo: replace placeholder name
+  DELETE FROM public.com_nordstrom_elwin_exposures WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id);
+  INSERT INTO public.com_nordstrom_elwin_exposures (SELECT * FROM duplicates.tmp_com_nordstrom_elwin_exposures);
 
 COMMIT;
 
 -- (d) move remaining duplicates to another table (optional)
 
-CREATE TABLE duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining -- todo: replace placeholder name
+CREATE TABLE duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining
   DISTKEY (root_id)
   SORTKEY (root_id)
-AS (SELECT root_id FROM (SELECT root_id, COUNT(*) AS count FROM public.com_nordstrom_elwin_exposures GROUP BY 1) WHERE count > 1); -- todo: replace placeholder name
+AS (SELECT root_id FROM (SELECT root_id, COUNT(*) AS count FROM public.com_nordstrom_elwin_exposures GROUP BY 1) WHERE count > 1);
 
 BEGIN;
 
-  INSERT INTO duplicates.com_nordstrom_elwin_exposures (SELECT * FROM public.com_nordstrom_elwin_exposures WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining)); -- todo: replace placeholder name
-  DELETE FROM public.com_nordstrom_elwin_exposures WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining); -- todo: replace placeholder name
+  INSERT INTO duplicates.com_nordstrom_elwin_exposures (SELECT * FROM public.com_nordstrom_elwin_exposures WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining));
+  DELETE FROM public.com_nordstrom_elwin_exposures WHERE root_id IN (SELECT root_id FROM duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining);
 
 COMMIT;
 
 -- (e) drop tables
 
-DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures; -- todo: replace placeholder name
-DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id; -- todo: replace placeholder name
-DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining; -- todo: replace placeholder name
+DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures;
+DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id;
+DROP TABLE IF EXISTS duplicates.tmp_com_nordstrom_elwin_exposures_id_remaining;
