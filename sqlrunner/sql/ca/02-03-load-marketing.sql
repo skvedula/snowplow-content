012fe9045a9c6d8577e577f26bbe7a7edb7088a7
White, David <David.K.White@nordstrom.com>
2016-10-19 10:54:02 -0700

    remove the unload statements copy/paste from load sql scripts

--- sqlrunner/sql/ca/02-03-load-marketing.sql
+++
@@ -1,3 +0,0 @@
-UNLOAD ('select * from public.events where etl_tstamp in (select max(etl_tstamp) from atomic.events)')
-to 's3://cxar-ato-bigdata/snowplow-prod/temp/'
-credentials 'aws_iam_role=arn:aws:iam::832038866117:role/46111/CXAR/ATO/' gzip;
