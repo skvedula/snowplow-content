b00f55ab809bcff101ac1b71c27ee32fdd06fde9
White, David <David.K.White@nordstrom.com>
2016-10-20 10:08:33 -0700

    update to load-events

--- sqlrunner/sql/ca/02-01-load-events.sql
+++ sqlrunner/sql/ca/02-01-load-events.sql
@@ -1,3 +1,3 @@
 COPY ato_sch.events
 FROM 's3://cxar-ato-bigdata/snowplow-prod/temp/events'
-CREDENTIALS 'aws_iam_role=arn:aws:iam::832038866117:role/46111/CXAR/ATO/';
+CREDENTIALS 'aws_iam_role=arn:aws:iam::832038866117:role/PC-ApplicationTeamRoles-EICustomerAnalyticsRedshif-ANX425Q0VUM7';
