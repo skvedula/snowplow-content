a82142fb0b6c5a2835839cb3b2e383cab540504a
Halliburton, Sean <Sean.Halliburton@nordstrom.com>
2016-10-19 21:43:45 -0700

    fixing col width, br_name inclusion, column ambiguity

--- sql/create_tables_duplicates.sql
+++ sql/create_tables_duplicates.sql
@@ -390,7 +390,7 @@ CREATE TABLE duplicates.nordstrom_remove_item (
 	mmp 					varchar(1)		encode lzo,
 	tag_id					varchar(10)	    encode lzo,
 	experiment_id			varchar(255)	encode lzo,
-	experiment_data			varchar(255)	encode lzo,
+	experiment_data			varchar(1000)	encode lzo,
 	FOREIGN KEY(root_id) REFERENCES duplicates.events(event_id)
 )
 DISTSTYLE KEY
