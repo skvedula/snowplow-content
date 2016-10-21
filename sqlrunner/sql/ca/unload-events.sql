7aa9aab1795c1a97c8961c801cd452c9fc43169c
White, David <David.K.White@nordstrom.com>
2016-10-21 14:50:54 -0700

    update public.events to atomic.temp_etl_tstamps

--- sqlrunner/sql/ca/unload-events.sql
+++ sqlrunner/sql/ca/unload-events.sql
@@ -2,7 +2,7 @@ UNLOAD ('SELECT *
 		FROM public.events 
 		WHERE etl_tstamp IN (
 			SELECT max(etl_tstamp) 
-			FROM public.events
+			FROM atomic.temp_etl_tstamps
 		);')
 TO 's3://cxar-ato-bigdata/snowplow-prod/CA/events/'
 CREDENTIALS 'aws_iam_role=arn:aws:iam::832038866117:role/a0007-Redshift-COPY';
