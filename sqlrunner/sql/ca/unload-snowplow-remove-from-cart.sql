d58ff0385a0bbfa4c16a0cfe6480dad771f00c6f
Halliburton, Sean <Sean.Halliburton@nordstrom.com>
2016-11-01 17:44:15 -0700

    updating for prod

---
+++ sqlrunner/sql/ca/unload-snowplow-remove-from-cart.sql
@@ -0,0 +1,16 @@
+UNLOAD ('SELECT root_id,
+       root_tstamp,
+       T1.derived_tstamp,
+       etl_tstamp_local,
+       sku,
+       name,
+       category,
+       unit_price,
+       quantity,
+       currency
+	FROM public.snowplow_remove_from_cart    T1,
+	atomic.temp_event_ids    T2
+      WHERE T1.root_id = T2.event_id;') 
+TO 's3://cxar-ato-bigdata/snowplow-prod/CA/snowplow_remove_from_cart/'
+CREDENTIALS 'aws_iam_role=arn:aws:iam::832038866117:role/a0007-Redshift-COPY'
+ESCAPE;
