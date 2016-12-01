0377e6ba07e352982c6786af3a8238b3d5cce2ab
Halliburton, Sean <Sean.Halliburton@nordstrom.com>
2016-11-30 21:10:52 -0800

    increasing authstatus column char count

--- sql/create_tables_atomic.sql
+++ sql/create_tables_atomic.sql
@@ -636,6 +636,36 @@ SORTKEY (root_tstamp);
 ALTER TABLE atomic.com_nordstrom_site_promos_1 owner to storageloader;
 
 
+--atomic.com_nordstrom_uids_1
+
+CREATE TABLE atomic.com_nordstrom_uids_1 (
+	-- Schema of this type
+	schema_vendor		varchar(128)  encode runlength not null,
+	schema_name			varchar(128)  encode runlength not null,
+	schema_format		varchar(128)  encode runlength not null,
+	schema_version		varchar(128)  encode runlength not null,
+	-- Parentage of this type
+	root_id				char(36)      encode raw not null,
+	root_tstamp			timestamp     encode raw not null,
+	ref_root			varchar(255)  encode runlength not null,
+	ref_tree			varchar(1500) encode runlength not null,
+	ref_parent			varchar(255)  encode runlength not null,
+	-- Properties of this type
+	coremetrics_id 		varchar(255)  encode lzo,
+	adobe_id 			varchar(255)  encode lzo,
+	elwin_id 			varchar(255)  encode lzo,
+	maxymiser_id 		varchar(255)  encode lzo,
+	authenticated 		varchar(255)  encode raw,
+	FOREIGN KEY(root_id) REFERENCES atomic.events(event_id)
+)
+DISTSTYLE KEY
+-- Optimized join to atomic.events
+DISTKEY (root_id)
+SORTKEY (root_tstamp);
+
+ALTER TABLE atomic.com_nordstrom_uids_1 owner to storageloader;
+
+
 --atomic.com_snowplowanalytics_snowplow_add_to_cart_1
 
 CREATE TABLE atomic.com_snowplowanalytics_snowplow_add_to_cart_1 (
