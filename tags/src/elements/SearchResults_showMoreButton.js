3915a4f712a03070dea58335efabc954ca0f0601
Andrew O'Neill <andrew.oneill@nordstrom.com>
2017-01-04 13:55:59 -0800

    tags/src/elements: switch to use flux action

--- tags/src/elements/SearchResults_showMoreButton.js
+++ tags/src/elements/SearchResults_showMoreButton.js
@@ -1,15 +1,15 @@
 /**
- * ShowMoreButton adds an event listener to the "show more" button on search
- * and browse pages. On click the button fires the appropriate coremetrics and
- * snowplow tags.
+ * ShowMoreButton adds a flux action listener to register clicks on the Show
+ * More button on search and browse pages.
  */
 const ShowMoreButton = () => {
-	const selector = `#npr-product-results-page section.npr-product-gallery > footer > button`;
-	const el = document.querySelector(selector)
-	el.addEventListener('click', e => {
-		cmCreateElementTag('ShowMoreClick', 'Search and Browse');
-		spCreateElementTag('ShowMoreClick', 'Search and Browse');
-	});
+    nord.core.register(p => {
+        if (p.action !== 'LoadMoreProducts') {
+            return;
+        }
+        cmCreateElementTag('ShowMoreClick', 'Search and Browse');
+        spCreateElementTag('ShowMoreClick', 'Search and Browse');
+    });
 }
 
 export default ShowMoreButton;
