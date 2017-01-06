93239add1e5f44e89d065170c27aa249b3f8d571
Halliburton, Sean <Sean.Halliburton@nordstrom.com>
2017-01-06 14:29:26 -0800

    fixing add to bag and add all to bag

--- tags/pages/BeautyBoard.js
+++ tags/pages/BeautyBoard.js
@@ -1,11 +1,11 @@
 import BeautyBoard_addBackToBoard from '../src/elements/BeautyBoard_addBackToBoard';
 import BeautyBoard_removeItem from '../src/elements/BeautyBoard_removeItem';
+import BeautyBoard_productClick from '../src/elements/BeautyBoard_productClick';
 import BeautyBoard_myRecsBeautyBoardRecsClick from '../src/elements/BeautyBoard_myRecsBeautyBoardRecsClick';
 import BeautyBoard_addToBag from '../src/elements/BeautyBoard_addToBag';
 import BeautyBoard_addAllToBag from '../src/elements/BeautyBoard_addAllToBag';
 import Cart_beautyBoardAddToBag from '../src/cart/Cart_beautyBoardAddToBag';
 import Cart_beautyBoardAddAllToBag from '../src/cart/Cart_beautyBoardAddAllToBag';
-import BeautyBoard_productClick from '../src/elements/BeautyBoard_productClick';
 
 function BeautyBoard_Tags() {
 	try {
@@ -25,7 +25,7 @@ function BeautyBoard_Tags() {
 				if (e.target.textContent === 'Remove This Item') BeautyBoard_removeItem();
 				else if (e.target.textContent === 'Add Back') BeautyBoard_addBackToBoard();
 			}
-			if (e.target.parentNode.className === 'product-href') BeautyBoard_productClick();
+			if (e.target.className === 'product-photo' || e.target.parentNode.className === 'product-href') BeautyBoard_productClick();
 		});
 	} catch(e) {
 		spLogError(e);
