dbe356572d24f39bde2fe99a1adf4b2bba90f382
Halliburton, Sean <Sean.Halliburton@nordstrom.com>
2017-01-06 09:13:04 -0800

    delegating events

--- tags/pages/GiftCardCheckout.js
+++ tags/pages/GiftCardCheckout.js
@@ -2,9 +2,8 @@ import GiftCardCheckout_edit from '../src/elements/GiftCardCheckout_edit';
 import GiftCardCheckout_paypal from '../src/elements/GiftCardCheckout_paypal';
 import GiftCardCheckout_remove from '../src/elements/GiftCardCheckout_remove';
 
-document.querySelector('#cart-content > div > div.cart-item-actions > ul > li:nth-child(1) > a').addEventListener('click', GiftCardCheckout_edit);
-
-document.querySelector('#order-paypal > img').addEventListener('click', GiftCardCheckout_paypal);
-document.querySelector('#cart-paypal > img').addEventListener('click', GiftCardCheckout_paypal);
-
-document.querySelector('#cart-content > div > div.cart-item-actions > ul > li:nth-child(2) > a').addEventListener('click', GiftCardCheckout_remove);
+document.addEventListener('click', function(e) {console.log(e.target);
+    if (e.target.className === 'cart-action-edit') GiftCardCheckout_edit();
+    if (e.target.className === 'cart-action-remove') GiftCardCheckout_remove();
+    if (e.target.nodeName === 'IMG' && /paypal/.test(e.target.parentNode.id)) GiftCardCheckout_paypal();
+});
